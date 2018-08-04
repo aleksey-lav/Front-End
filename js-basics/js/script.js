f8c844b3e5f0886e0046cb6439965de165623ee6
aleksey leichenko <ol.leichenko@gmail.com>
2018-08-04 13:54:20 +0300

    script

---
+++ js-basics/js/script.js
@@ -0,0 +1,76 @@
+// let a = 10;
+//
+// let str = 'string';
+//
+// let boolean = false;
+//
+// let nullvar = null;
+//
+// let undefvar;
+//
+// let obj = {};
+
+// alert('Hello JS');
+
+// console.log(NaN, Infinity);
+
+// console.log('123');
+
+// console.log(Number(false));
+
+// console.log(String(true));
+
+// console.log(Boolean());
+
+// alert('Это алерт');
+
+// let message = prompt('Это промпт','');
+
+// console.log(message);
+
+// let suffer = confirm('ничего не поделаешь, придется страдать');
+//
+// console.log(suffer);
+
+// console.log(-(2+4)/0);
+
+// console.log(2+'2' === 22);
+
+// console.log(true+'');
+
+// console.log(4-2-'6');
+//
+// console.log(4*'6');
+//
+// console.log(6/'2');
+//
+// console.log(3, typeof 3);
+// console.log('string', typeof 'string');
+// console.log(true, typeof true);
+// console.log(null, typeof null);
+// console.log(undefined, typeof undefined);
+// console.log(NaN, typeof NaN);
+// console.log('object', typeof {});
+// // console.log('function',typeof function(){});
+//
+// if (true) {
+// console.log(true);
+// } else {
+//
+// }
+
+
+// for (let i = 0;i<10;i++) {
+//     console.log(i);
+// }
+
+// let a = 1;
+// while (a<5) {
+//     console.log(a);
+//     a++;
+// }
+// do {
+//     a++;
+//     console.log(a);
+// } while (a<5);
+
